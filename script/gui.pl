4765d21cccb35f561fffcefa2f22e4561f2ac02d
Brian Duggan <brian.duggan@nasa.gov>
2014-04-12 14:13:14 -0600

    start/stop, filter files

--- script/gui.pl
+++ script/gui.pl
@@ -1,27 +1,12 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
+use Mojo::JSON;
+use Mojo::Util qw/sha1_sum/;
+use Time::HiRes qw/gettimeofday/;
+
+my $j = Mojo::JSON->new();
 
 get '/' => 'index';
-
-get '/data.tsv' => sub {
-    my $c = shift;
-    $c->res->headers->content_type('text/plain');
-    my $text = join "\n",map "$_\t$_", 1..14;
-    $c->render( text => "name\tvalue\n$text");
-};
-
-get '/data.json' => sub {
-    my $c = shift;
-    Mojo::IOLoop->timer( 0.1 => 
-        sub {
-            $c->render(json => [
-                { name => 'a', value => 10 },
-                { name => 'b', value => 22 },
-                { name => 'c', value => 142 },
-                ]);
-        });
-    $c->render_later;
-};
 
 get '/seq' => sub {
   my $c = shift;
@@ -29,11 +14,18 @@ get '/seq' => sub {
   $c->res->headers->content_type('text/event-stream');
   my $i = 1;
   my $id = Mojo::IOLoop->singleton->recurring(
-    0.5 => sub {
-      my $s = '';
-      for (1..10) {
-          $s .= $_ if rand 10 > 5;
+    1 => sub {
+      my @new_files;
+      for (1..1 + (int rand 10_000)) {
+        push @new_files,
+            {
+                filename => "file-" . rand,
+                created  => scalar gettimeofday(),
+                sha1     => sha1_sum(scalar gettimeofday().(rand 2).(rand 1)),
+            };
       }
+
+      my $s = $j->encode(\@new_files);
       $c->write("event:seq\ndata: $s\n\n");
     }
   );
